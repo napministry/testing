2c60b0e025303d6ec860c7720458db0088807cb8
theresa <ngn.theresa@gmail.com>
2021-01-28 13:40:58 -0600

    initial commit

---
+++ src/testing/Main.java
@@ -0,0 +1,13 @@
+package testing;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi hello");
+		Scanner input = new Scanner(System.in); 
+	}
+
+}
